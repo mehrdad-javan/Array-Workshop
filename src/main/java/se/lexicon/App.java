24f44288b75b9dec60d1f8ab9345e8b46b2677a3
Mehrdad Javan <mehrdad.javan@lexicon.se>
2024-06-19 08:08:33 +0200

    execute methods in App.java

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,13 @@
 package se.lexicon;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+    public static void main(String[] args) {
+
+        int size = NameRepository.getSize();
+        System.out.println(size);
+        NameRepository.setNames(new String[]{"Erik Svensson", "Mehrdad Javan"});
+        System.out.println(NameRepository.getSize());
+        // call more methods as needed
     }
 }
